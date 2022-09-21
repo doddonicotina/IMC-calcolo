d16d28bc0d3f06fe460da379eb60a973fb1b860e
doddonicotina <74264293+doddonicotina@users.noreply.github.com>
2022-09-21 10:08:27 +0200

    aiuto

--- IMC.java
+++ IMC.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("CALCOLO IMC");
+        System.out.println("Inserire peso: ");
+        float weight = scanner.nextFloat();
+
+        scanner.nextLine();
+
+        System.out.println("Inserire altezza: ");
+        float height = scanner.nextFloat();
+
+        scanner.nextLine();
+
+        float IMC = weight/height*height;
+        String status;
+        if (IMC < 16){
+                status = "anoressico";
+        }else if (IMC >= 16 && IMC < 19){
+                status = "anoressico";
+        }else if (IMC >= 19 && IMC < 24){
+                status = "normopeso";
+        }else if (IMC >25 && IMC <30){
+                status = "sovrappeso";
+        }else{
+                status = "obeso";
+        }
+    
+        System.out.println("sei"+status);
+
+    }
+}
